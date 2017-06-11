1ca19e3ff9bd44a0ddad9fd53a2a7f4fa9ec35be
Devwork <285088180@qq.com>
2017-06-11 23:35:25 +0800

    update

--- gulp/tasks/development/jsconcat.js
+++ gulp/tasks/development/jsconcat.js
@@ -1,48 +1,23 @@
-var fs             = require('fs')
 var path           = require('path')
+var folders        = require('gulp-folders')
 var normalize      = require('normalize-path')
 var gulp           = require('gulp')
 var concat         = require('gulp-concat')
 var sourcemap      = require('gulp-sourcemaps')
-var rename         = require('gulp-rename')
 var config         = require('../../config').js
 
 
 if (!config) return
 var exclude = path.normalize('!**/{' + config.excludeFolders.join(',') + '}/**');
 
-var list = [];
-
-function getFolders(dir) {
-  fs.readdirSync(dir)
-    .filter(function(file) {
-      if (fs.statSync(path.join(dir, file)).isDirectory()) {
-        getFolders(path.join(dir, file));
-        list.push({
-          fullPath: normalize(path.join(dir, file)).replace('src/', '/'),
-          parentFolderName: path.join(dir, file).split(path.sep).slice(-1)[0]
-        });
-      }
-    });
-  return list;
-}
-
-gulp.task('jsconcat', function() {
-  return getFolders(config.folder).map(function(subfolder) {
-    console.log(exclude);
-    return gulp.src(subfolder.fullPath + '/*.js')
-      .pipe(sourcemap.init())
-      .pipe(concat(subfolder.parentFolderName + '.js'))
-      .pipe(sourcemap.write())
-      .pipe(gulp.dest(config.dest));
-  });
-});
-/**
- * Copy and minimize JS files
- */
-// gulp.task('jsconcat', function() {
-//   return gulp.src(config.src)
-//     .pipe(sourcemap.init())
-//     .pipe(sourcemap.write('./'))
-//     .pipe(gulp.dest(config.dest));
-// });
+gulp.task('jsconcat', folders(config.folder, function(folder) {
+  //This will loop over all folders inside pathToFolder main, secondary 
+  //Return stream so gulp-folders can concatenate all of them 
+  //so you still can use safely use gulp multitasking 
+  console.log(exclude);
+  return gulp.src([path.join(config.folder, folder, '*.js'), exclude])
+    .pipe(sourcemap.init())
+    .pipe(concat(folder + '.js'))
+    .pipe(sourcemap.write('.'))
+    .pipe(gulp.dest(config.dest));
+}));
